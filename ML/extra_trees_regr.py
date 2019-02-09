72ab3fc2d99d7f7f1676cf3ce720aa6be8555bf1
KacperKubara <kjk1u17@soton.ac.uk>
2019-02-09 18:02:38 +0000

    Regression Class finished

--- ML/extra_trees_regr.py
+++ ML/extra_trees_regr.py
@@ -8,5 +8,28 @@ class ExtraTreesRegr:
         self.filename = "ml_model.sav"
         self.model = pickle.load(open(self.filename, 'rb'))
     
-    def predict(self, arg_list):   
-        return self.model.predict(arg_list)
+    def predict(self, arg_list = [[0,0,0,0]], coordinates = [[0, 0]]):
+        if self.is_input_alright(arg_list, coordinates):
+            sum =  self.predict_hard_coded(coordinates) + self.model.predict(arg_list)  
+            return sum
+        else: 
+            return "Incorrect Input!"
+    
+    def is_input_alright(self, arg_list, coordinates):
+        if len(arg_list) == len(coordinates):
+            if len(arg_list[0]) == 4 and len(coordinates[0]) == 2:
+                return True
+        else: 
+            return False
+
+    def predict_hard_coded(self, coordinates):
+        adjustment_xD = list()
+        for coords in coordinates:
+            if coords[0] > 0 and coords[0] > 0:
+                adjustment_xD.append(100)
+            else:
+                adjustment_xD.append(0)
+                
+        return adjustment_xD
+        
+
