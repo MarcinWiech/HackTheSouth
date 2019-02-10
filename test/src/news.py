0832c75f6ed63c5fd89ca74fed8b61472b40fa92
Konstantinos Tsivis <kostaspapadopoulos369@gmail.com>
2019-02-10 09:53:18 +0000

    added news json

---
+++ test/src/news.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+def getNews():
+    resp = requests.get('https://newsapi.org/v2/everything?q=wildfire&apiKey=' + 'b9d10b8dcf8f440d90e024091b8429a1')
+    j = resp.json()
+    return j
+
+if __name__ == '__main__':
+    s = getNews()
+    print(json.dumps(s))
+
